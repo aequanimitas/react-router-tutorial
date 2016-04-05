b4191ce6674a65522d76b70cfaac94e96c37ce8d
aequanimitas <htavenido@gmail.com>
2016-04-06 01:33:06 +0800

    02 - Rendering a route02

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom'
+import { Router, Route, hashHistory } from 'react-router'
 
 let App = React.createClass({
   render() {
@@ -7,7 +8,23 @@ let App = React.createClass({
   }
 })
 
+
+let About = React.createClass({
+  render() {
+    return <div>About!</div>
+  }
+})
+
+let Repos = React.createClass({
+  render() {
+    return <div>Repos!</div>
+  }
+})
 render(
-  <App />,
+  <Router history={hashHistory}>
+   <Route path="/" component={App} /> 
+   <Route path="/repos" component={Repos} /> 
+   <Route path="/about" component={About} /> 
+  </Router>,
   document.getElementById('app')
 )
